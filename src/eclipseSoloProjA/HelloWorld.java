35e312a1508f120fefebf9f3465d74113ef93fae
marissa1 <marissa1@localhost>
2025-02-03 11:39:52 -0500

    Another commit

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("A new line statement");
+System.out.println("A second line statement");
 	}
 
 }
